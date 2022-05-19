cfd75815da856ad4ab9db35ff5df706392067502
Krischal-Om-Pote <krischalom.pote@apexcollege.edu.np>
2022-05-19 07:16:54 +0545

    Update

--- ecommerce_lab1/settings.py
+++ ecommerce_lab1/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'product_module',
+    'brand_module',
+    'category_module',
 ]
 
 MIDDLEWARE = [
