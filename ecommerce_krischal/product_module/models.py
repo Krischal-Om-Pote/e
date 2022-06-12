0cbd87b411072758597d2566e7041c4f02b2b445
Krischal-Om-Pote <krischalom.pote@apexcollege.edu.np>
2022-06-12 13:32:25 +0545

    update product

--- ecommerce_krischal/product_module/models.py
+++ ecommerce_krischal/product_module/models.py
@@ -2,25 +2,25 @@ from django.db import models
 from django.utils.html import mark_safe
 
 # Create your models here.
-
-
 class Brand(models.Model):
     name = models.CharField(max_length=200)
     is_active = models.BooleanField()
+    def __str__(self):
+             return self.name
 
 
 class Category(models.Model):
-
     name = models.CharField(max_length=200)
     is_active = models.BooleanField()
 
     class Meta:
-
         verbose_name_plural = "Categories"
+    def __str__(self):
+             return self.name
+
 
 
 class Product(models.Model):
-
     name = models.CharField(max_length=200)
     price = models.FloatField()
     quantity = models.IntegerField()
@@ -31,9 +31,12 @@ class Product(models.Model):
     registered_on = models.DateTimeField()
     is_active = models.BooleanField()
 
+
     def image_tag(self):
         return mark_safe(f'<img src="{self.image_url}" width="50" height="50" />')
     image_tag.short_description = "Product"
 
     def __str__(self):
-        return self.name
+             return self.name
+
+
